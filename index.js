878df447f6067f82a485197ba46a5971245d9e2b
najira <najira_daniela@hotmail.com>
2024-05-24 23:40:53 -0400

    entrega de imagenes con jimp

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import express from 'express'
 import 'dotenv/config'
+import Jimp from 'jimp'
+
 
 const app = express()
+
+
+app.get('/image', async (req, res) => {
+
+    const image = await Jimp.read('https://picsum.photos/400')
+    //agregar config a la imagen:
+    const buffer = await image
+        .resize(350, 350)
+        .grayscale()
+        .getBufferAsync(Jimp.MIME_JPEG)
+
+    res.set("Content-Type", "image/jpeg")
+    return res.send(buffer)
+
+})
+
 
 
 
